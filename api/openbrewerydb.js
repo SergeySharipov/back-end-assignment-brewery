ce9d9666d097456e1357d65378480843433070ce
SergeySharipov <sharipov14@gmail.com>
2022-03-10 21:34:31 -0500

    Add cache for API requests to Open Brewery DB

--- api/openbrewerydb.js
+++ api/openbrewerydb.js
@@ -1,12 +1,16 @@
 const axios = require('axios')
+const cachios = require('cachios')
 const { OPEN_BREWERY_DB_API_URL } = require('../constants/openbrewerydb.constants')
 
-const api = axios.create({
+const axiosInstance = axios.create({
   baseURL: OPEN_BREWERY_DB_API_URL
 })
 
+const api = cachios.create(axiosInstance)
+
+// Send a GET request to openbrewerydb REST API, cache duration 10s
 const getBreweries = async (byCity, byState, byType) => {
-  return api.get('/breweries', { params: { by_city: byCity, by_state: byState, by_type: byType } })
+  return api.get('/breweries', { params: { ttl: 10, by_city: byCity, by_state: byState, by_type: byType } })
 }
 
 module.exports = { getBreweries }
